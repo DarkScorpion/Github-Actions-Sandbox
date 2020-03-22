5e50a81440aa440aa51dd4e7bb0c7d8a4a56f700
Alex Smith <iqwz@ya.ru>
2020-03-23 00:24:49 +0300

    server and tests

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send('ok');
+});
+
+//Query routes
+app.get('/query', (req, res) => {
+  var q = req.query;
+  var p1 = q.p1 || '???';
+  var p2 = q.p2 || '???';
+  var result = p1 + ' ' + p2;
+  res.send(result);
+});
+
+//Param routes
+app.get('/echo/:p1', (req, res) => {
+  var p1 = req.params.p1;
+  res.send(p1);
+});
+
+//Types routes
+app.all('/types', (req, res) => {
+  res.send(req.method + ' ok');
+});
+
+//Route404
+app.get('/route404', (req, res) => {
+  res.status(404).send('404');
+});
+
+//Other routes
+app.all('*', (req, res) => {
+  console.log('Warning: strange route: %s %s', req.path, req.method);
+  res.status(404).send('=?=');
+});
+
+module.exports = app;
---
+++ test/t1 check server.js
@@ -0,0 +1,87 @@
+'use strict';
+var assert  = require('assert');
+var request = require('request');
+
+var app = require('../app.js');
+
+const PORT = 3001;
+const SERVER = app.listen(PORT);
+const BASE_URL = 'http://localhost:' + PORT;
+
+describe('Check server', () => {
+  it('/index GET', (done) => {
+    testRequest('/', 'get', (err, res, body) => {
+      assert.equal(err, null);
+      assert.equal(res.statusCode , 200);
+      assert.equal(body, 'ok');
+      done();
+    });
+  });
+
+  it('/types GET', (done) => {
+    testRequest('/types', 'get', (err, res, body) => {
+      assert.equal(body, 'GET ok');
+      done();
+    });
+  });
+
+  it('/types POST', (done) => {
+    testRequest('/types', 'post', (err, res, body) => {
+      assert.equal(body, 'POST ok');
+      done();
+    });
+  });
+
+  it('/types PUT', (done) => {
+    testRequest('/types', 'put', (err, res, body) => {
+      assert.equal(body, 'PUT ok');
+      done();
+    });
+  });
+
+  it('/types DEL', (done) => {
+    testRequest('/types', 'delete', (err, res, body) => {
+      assert.equal(body, 'DELETE ok');
+      done();
+    });
+  });
+
+  it('/echo GET', (done) => {
+    var echoData = '123'
+    testRequest('/echo/'+echoData, 'get', (err, res, body) => {
+      assert.equal(body, echoData);
+      done();
+    });
+  });
+
+  it('/query GET', (done) => {
+    var query = '?p1=aaa&p2=bbb';
+    testRequest('/query'+query, 'get', (err, res, body) => {
+      assert.equal(body, 'aaa bbb');
+      done();
+    });
+  });
+
+  it('/route404 GET', (done) => {
+    testRequest('/route404', 'get', (err, res, body) => {
+      assert.equal(err, null);
+      assert.equal(res.statusCode , 404);
+      assert.equal(body, '404');
+      done();
+    });
+  });
+
+  after( () => {
+    SERVER.close();
+  });
+
+});
+
+function testRequest(route, method, cb) {
+  request({
+    url: BASE_URL+route,
+    method: method,
+  }, (err, res, body) => {
+    cb(err, res, body);
+  });
+}
